8d83a935bac3f23af89efa47ce8cacfea7d68bf0
kadeem-online <kadeemalexander254@gmail.com>
2026-04-08 16:53:30 +0300

    refactor: created the game bootstrap code

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,28 @@
-console.log("Flappy Bird Clone");
+// third party imports
+import Phaser from "phaser";
+
+// local application imports
+import * as CFG from "./config.ts";
+
+function runGame(): Phaser.Game {
+	const config: Phaser.Types.Core.GameConfig = {
+		width: CFG.GAME_WIDTH,
+		height: CFG.GAME_HEIGHT,
+
+		parent: "game",
+
+		scale: {
+			mode: Phaser.Scale.FIT,
+			autoCenter: Phaser.Scale.CENTER_BOTH,
+		},
+	};
+
+	const game = new Phaser.Game(config);
+	return game;
+}
+
+function onPageLoad() {
+	runGame();
+}
+
+window.addEventListener("load", onPageLoad);
