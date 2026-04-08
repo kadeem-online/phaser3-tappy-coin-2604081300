c2495986226aaf6fdb1dda039245e5f3c961a9e7
kadeem-online <kadeemalexander254@gmail.com>
2026-04-08 17:28:45 +0300

    refactor: allow setting width and height in env

--- src/config.ts
+++ src/config.ts
@@ -3,5 +3,5 @@
  * This file contains configuration values for the game.
  *
  */
-export const GAME_WIDTH: number = 480;
-export const GAME_HEIGHT: number = 360;
+export const GAME_WIDTH: number = import.meta.env.VITE_GAME_WIDTH || 480;
+export const GAME_HEIGHT: number = import.meta.env.VITE_GAME_HEIGHT || 360;
