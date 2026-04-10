2ba75e748543e6beda3595109a0240045e323005
kadeem-online <kadeemalexander254@gmail.com>
2026-04-10 09:54:10 +0300

    chore: added a variable to manage scene names

--- src/config.ts
+++ src/config.ts
@@ -5,3 +5,8 @@
  */
 export const GAME_WIDTH: number = import.meta.env.VITE_GAME_WIDTH || 480;
 export const GAME_HEIGHT: number = import.meta.env.VITE_GAME_HEIGHT || 360;
+
+type SceneKeys = "prototype";
+export const SCENE_KEYS: Record<SceneKeys, string> = Object.freeze({
+	prototype: "PROTOTYPE_LEVEL",
+});
