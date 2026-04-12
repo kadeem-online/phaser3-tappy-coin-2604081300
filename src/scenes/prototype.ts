b7abd69e34871688cb4e7110b3c02321ee83df80
kadeem-online <kadeemalexander254@gmail.com>
2026-04-12 20:33:26 +0300

    chore: added basic tap to lift control for the player

--- src/scenes/prototype.ts
+++ src/scenes/prototype.ts
@@ -24,8 +24,31 @@ function create_player(scene: Phaser.Scene): Phaser.Physics.Arcade.Sprite {
 	return player;
 }
 
+function create_player_input_zone(
+	scene: Phaser.Scene,
+): Phaser.GameObjects.Zone {
+	const zone = new Phaser.GameObjects.Zone(
+		scene,
+		scene.scale.width / 2,
+		scene.scale.height / 2,
+		scene.scale.width,
+		scene.scale.height,
+	);
+
+	const touchableArea = new Phaser.Geom.Rectangle(
+		0,
+		0,
+		scene.scale.width,
+		scene.scale.height,
+	);
+	zone.setInteractive(touchableArea, Phaser.Geom.Rectangle.Contains);
+
+	return zone;
+}
+
 export default class PrototypeLevel extends Phaser.Scene {
 	player: Phaser.Physics.Arcade.Sprite;
+	tapZone: Phaser.GameObjects.Zone;
 
 	constructor() {
 		super(CFG.SCENE_KEYS.prototype);
@@ -37,6 +60,15 @@ export default class PrototypeLevel extends Phaser.Scene {
 		const playerStartPos = new Phaser.Math.Vector2(40, this.scale.height / 2);
 		this.player.setPosition(playerStartPos.x, playerStartPos.y);
 		this.player.setCollideWorldBounds(true);
-		this.player.setGravityY(20);
+		this.player.setGravityY(100);
+
+		this.tapZone = create_player_input_zone(this);
+		this.tapZone.on("pointerdown", this.handleTapZoneInput, this);
+	}
+
+	handleTapZoneInput() {
+		const tapVelocity: number = -50;
+		this.player.setVelocityY(tapVelocity);
+		return;
 	}
 }
