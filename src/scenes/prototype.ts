e8e3f6cd9384a3e10afbbe4219331248e9e455f2
kadeem-online <kadeemalexander254@gmail.com>
2026-04-14 07:00:15 +0300

    feat: created an Obstacle and ObstaclePoolManager class.

--- src/scenes/prototype.ts
+++ src/scenes/prototype.ts
@@ -46,9 +46,93 @@ function create_player_input_zone(
 	return zone;
 }
 
+class Obstacle extends Phaser.Physics.Arcade.Image {
+	declare body: Phaser.Physics.Arcade.Body;
+
+	constructor(
+		scene: Phaser.Scene,
+		x: number,
+		y: number,
+		texture: string | Phaser.Textures.Texture,
+		frame: string | number,
+	) {
+		super(scene, x, y, texture, frame);
+	}
+}
+
+/**
+ * helper function that generates the temporary obstacle texture and registers
+ * it in the scene.
+ *
+ * @param {Phaser.Scene} scene - The scene the pool manager will be instantiated
+ * into.
+ */
+function obstacle_pool_manager_register_temporary_texture(
+	scene: Phaser.Scene,
+): string {
+	const OBSTACLE_TEXUTRE_KEY: string = "TEMPORARY_OBSTACLE_TEXTURE";
+
+	if (scene.textures.exists(OBSTACLE_TEXUTRE_KEY)) {
+		return OBSTACLE_TEXUTRE_KEY;
+	}
+
+	const TEXTURE_WIDTH = 32;
+	const TEXTURE_HEIGHT = scene.scale.height;
+
+	const texture = scene.add.graphics();
+	texture.fillStyle(0xff0000, 1);
+	texture.fillRect(0, 0, TEXTURE_WIDTH, TEXTURE_HEIGHT);
+
+	texture.generateTexture(OBSTACLE_TEXUTRE_KEY, TEXTURE_WIDTH, TEXTURE_HEIGHT);
+	texture.destroy();
+
+	return OBSTACLE_TEXUTRE_KEY;
+}
+
+/**
+ * Helper function that handles any actions that need to be taken on newly
+ * created objects in the group.
+ */
+function obstalce_pool_manager_create_callback(
+	obstacle: Phaser.GameObjects.GameObject,
+) {
+	if (obstacle instanceof Obstacle) {
+		obstacle.setVisible(false);
+		obstacle.setActive(false);
+		obstacle.body.enable = false;
+		obstacle.body.setImmovable(true);
+	}
+
+	return;
+}
+
+class ObstaclePoolManager extends Phaser.Physics.Arcade.Group {
+	constructor(scene: Phaser.Scene) {
+		const KEY: string = obstacle_pool_manager_register_temporary_texture(scene);
+		const MAX_POOL_SIZE: number = 30;
+
+		super(scene.physics.world, scene, {
+			classType: Obstacle,
+			key: KEY,
+			maxSize: MAX_POOL_SIZE,
+			active: false,
+			visible: false,
+			quantity: 0,
+			createCallback: obstalce_pool_manager_create_callback,
+		});
+		this._generate_obstacle_texture();
+	}
+
+	/**
+	 * Creates the temporary obstacle texture if it does not yet exist.
+	 */
+	_generate_obstacle_texture() {}
+}
+
 export default class PrototypeLevel extends Phaser.Scene {
 	player!: Phaser.Physics.Arcade.Sprite;
 	tapZone!: Phaser.GameObjects.Zone;
+	obstaclePool!: ObstaclePoolManager;
 
 	constructor() {
 		super(CFG.SCENE_KEYS.prototype);
@@ -64,6 +148,8 @@ export default class PrototypeLevel extends Phaser.Scene {
 
 		this.tapZone = create_player_input_zone(this);
 		this.tapZone.on("pointerdown", this.handleTapZoneInput, this);
+
+		this.obstaclePool = new ObstaclePoolManager(this);
 	}
 
 	handleTapZoneInput() {
